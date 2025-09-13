99ce9c3f932f81a38e573eb5cc1bc57d035e51e6
katyalalavanya <lavanyakatyala0812@gmail.com>
2025-09-13 18:39:19 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "lavanya.devops.need.bucket"
+  bucket = "lavanya77.devops.need.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "eu-north-1"
-bucket = "lavanya.devops.need.bucket"
+bucket = "lavanya77.devops.need.bucket"
 key = "prod/terraform.tfstate"
 }
 }
