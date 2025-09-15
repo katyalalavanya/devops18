333f778e1ec1be707bf7ff5d5e9ef84c7f88a8aa
katyalalavanya <lavanyakatyala0812@gmail.com>
2025-09-15 13:02:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "lavanya99.lav.devops.need.bucket"
+bucket = "lavanya99.lav.devops.project.bucket"
 key = "prod/terraform.tfstate"
 }
 }
